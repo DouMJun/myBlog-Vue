4856a226e06e371ccc7a1ea39a130d0a83cfdb1d
DouMJun <1228990775@qq.com>
2020-11-19 19:16:18 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,11 @@ set -e
 git add -A
 git commit -m 'deploy'
 
-# 获取更新后的时间
-# node getTime.js
 
 
-git add -A
-git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 # https://doumjun.github.io/vuepress.blog/
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
+
 git push -f git@github.com:DouMJun/myBlog-Vue.git master
